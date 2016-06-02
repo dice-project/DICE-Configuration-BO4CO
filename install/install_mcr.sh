0dd0e24e799ffbe606115f41127e26b102595f3e
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-02 13:00:16 +0100

    editted install mcr

--- install/install_mcr.sh
+++ install/install_mcr.sh
@@ -1,4 +1,4 @@
 mkdir mcr && cd mcr
 wget http://uk.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/glnxa64/MCR_R2015a_glnxa64_installer.zip &&\
-unzip MCR_R2013a_glnxa64_installer.zip &&\
+unzip MCR_R2015a_glnxa64_installer.zip &&\
 sudo ./install -mode silent -agreeToLicense yes
