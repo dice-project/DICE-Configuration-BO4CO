f13f1fb7ab0d9639795781ea0a5c8be8eaf83017
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-07 20:23:21 +0100

    added functions for executing workload

---
+++ src/integrated/execute_load_trace.m
@@ -0,0 +1,9 @@
+function execute_load_trace()
+
+
+% Authors: Pooyan Jamshidi (pooyan.jamshidi@gmail.com)
+% The code is released under the FreeBSD License.
+% Copyright (C) 2016 Pooyan Jamshidi, Imperial College London
+
+
+end
