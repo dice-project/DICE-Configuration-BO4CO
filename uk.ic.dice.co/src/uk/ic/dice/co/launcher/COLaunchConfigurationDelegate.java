26496213771821a77ab08656b35c22373f920815
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-11 18:35:36 +0100

    edited launch configuration delegate

--- uk.ic.dice.co/src/uk/ic/dice/co/launcher/COLaunchConfigurationDelegate.java
+++ uk.ic.dice.co/src/uk/ic/dice/co/launcher/COLaunchConfigurationDelegate.java
@@ -8,6 +8,10 @@ import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 
 public class COLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
 
+	public static final String INPUT_FILE = "INPUT_CONFIG"; //$NON-NLS-1$
+	public static final String EXECUTION_MODE = "EXECUTION_MODE"; //$NON-NLS-1$
+
+
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
 			throws CoreException {
