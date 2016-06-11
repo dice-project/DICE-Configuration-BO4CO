2b228264af9ace5f57fb63bdda073f447085a14e
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-11 01:08:59 +0100

    added few more controls to the launcher

--- uk.ic.dice.co.ui/src/uk/ic/dice/co/ui/launcher/MainLaunchConfigTab.java
+++ uk.ic.dice.co.ui/src/uk/ic/dice/co/ui/launcher/MainLaunchConfigTab.java
@@ -4,6 +4,18 @@ import java.io.File;
 import java.net.URI;
 import java.util.Set;
 
+import org.apache.commons.validator.routines.DomainValidator;
+import org.apache.commons.validator.routines.InetAddressValidator;
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.conversion.StringToNumberConverter;
+import org.eclipse.core.databinding.observable.Observables;
+import org.eclipse.core.databinding.observable.map.IObservableMap;
+import org.eclipse.core.databinding.observable.map.WritableMap;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
@@ -11,6 +23,9 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.jface.databinding.fieldassist.ControlDecorationSupport;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
@@ -23,6 +38,8 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
 
 
@@ -63,8 +80,22 @@ public class MainLaunchConfigTab extends AbstractLaunchConfigurationTab {
 	protected TableViewer viewer;
 	protected TableViewerColumn varViewerColumn;
 	protected TableViewerColumn valueViewerColumn;
+	
+	private IObservableMap preferencesMap = new WritableMap();
+	private DataBindingContext context;
 
+	private final class HostValidator implements IValidator {
+		@Override
+		public IStatus validate(Object value) {
+			if (InetAddressValidator.getInstance().isValid(value.toString()) || 
+					DomainValidator.getInstance(true).isValid(value.toString())) {
+				return ValidationStatus.ok();
+			}
+			return ValidationStatus.error("Invalid host name or IP address");
+		}
+	}
 
+	
 	@Override
 	public void createControl(Composite parent) {		
 		Composite topComposite = new Composite(parent, SWT.NONE);
@@ -72,6 +103,9 @@ public class MainLaunchConfigTab extends AbstractLaunchConfigurationTab {
 
 		GridData buttonsGridData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
 		buttonsGridData.widthHint = 100;
+		
+		GridData labelGridData = new GridData(SWT.FILL, SWT.TOP, false, false);
+		labelGridData.widthHint = 100;
 
 		{ // Configuration Group
 			Group group = new Group(topComposite, SWT.NONE);
@@ -141,6 +175,52 @@ public class MainLaunchConfigTab extends AbstractLaunchConfigurationTab {
 				};
 			});
 
+		}
+		
+		
+		{ // Test platform address
+			
+			final Group serverGroup = new Group(topComposite, SWT.NONE);
+			serverGroup.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+			serverGroup.setText("Testing server");
+			serverGroup.setLayout(new GridLayout(2, false));
+
+			final Label serverLabel = new Label(serverGroup, SWT.NONE);
+			serverLabel.setText("Host:");
+			serverLabel.setLayoutData(labelGridData);
+
+			final Text serverText = new Text(serverGroup, SWT.BORDER);
+			serverText.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+
+			final Label portLabel = new Label(serverGroup, SWT.NONE);
+			portLabel.setText("Port:");
+			portLabel.setLayoutData(labelGridData);
+
+			final Spinner portSpinner = new Spinner(serverGroup, SWT.BORDER);
+			portSpinner.setMinimum(0);
+			portSpinner.setMaximum(0xFFFF);
+			portSpinner.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
+			
+			// Data binding
+			//
+			// Observables for the Widgets
+			final ISWTObservableValue hostTextObservable =  WidgetProperties.text(SWT.Modify).observe(serverText);
+			final ISWTObservableValue portSpinnerObservable = WidgetProperties.selection().observe(portSpinner);
+
+			// Observables for the data model
+			final IObservableValue hostObservable = Observables.observeMapEntry(preferencesMap, "host");
+			final IObservableValue portObservable = Observables.observeMapEntry(preferencesMap, "port");
+
+			// Observables for the data model
+			// Host binding
+			final Binding hostBinding = context.bindValue(hostTextObservable, hostObservable,
+					new UpdateValueStrategy().setBeforeSetValidator(new HostValidator()), null);
+			ControlDecorationSupport.create(hostBinding, SWT.TOP | SWT.LEFT);
+			// Port binding
+			context.bindValue(portSpinnerObservable, portObservable, 
+					null,
+					new UpdateValueStrategy().setConverter(StringToNumberConverter.toInteger(true)));
+			
 		}
 
 		setControl(topComposite);
