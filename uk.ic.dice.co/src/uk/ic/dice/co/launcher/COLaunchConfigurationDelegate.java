3682233ac4617445d13cb3a0650a178e4804082f
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-11 15:45:15 +0100

    first commit of the CO remote control

---
+++ uk.ic.dice.co/src/uk/ic/dice/co/launcher/COLaunchConfigurationDelegate.java
@@ -0,0 +1,18 @@
+package uk.ic.dice.co.launcher;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+
+public class COLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
