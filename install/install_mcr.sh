200c8a46aedd62792f189953e6dd5dad778e0650
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-15 16:40:06 +0100

    editted mcr install

--- install/install_mcr.sh
+++ install/install_mcr.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # script for installing the MATLAB runtime environment
 #
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install g++ libxmu6 libxt6 libxpm4 libxp6 -y
-mkdir mcr && cd mcr
+sudo apt-get update && sudo apt-get upgrade -y &&\
+sudo apt-get install g++ libxmu6 libxt6 libxpm4 libxp6 zip -y &&\
+mkdir mcr && cd mcr &&\
 wget http://uk.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/glnxa64/MCR_R2015a_glnxa64_installer.zip &&\
 unzip MCR_R2015a_glnxa64_installer.zip &&\
 sudo ./install -mode silent -agreeToLicense yes
