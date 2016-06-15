3d107c3ecd629aa731557ad842e114196fbd4563
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-15 15:17:40 +0100

    updated mcr install script

--- uk.ic.dice.co/src/uk/ic/dice/co/launcher/COLaunchConfigurationDelegate.java
+++ uk.ic.dice.co/src/uk/ic/dice/co/launcher/COLaunchConfigurationDelegate.java
@@ -3,6 +3,9 @@ package uk.ic.dice.co.launcher;
 import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.stream.FileImageInputStream;
 
@@ -13,6 +16,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
@@ -34,7 +38,16 @@ public class COLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
 		}
 		try {
 			monitor.beginTask("Configuration Optimization", IProgressMonitor.UNKNOWN);
+			
+			Map<String, String> optimizationAttrs = new HashMap<>();
+			optimizationAttrs.put(DebugPlugin.ATTR_LAUNCH_TIMESTAMP, Calendar.getInstance().getTime().toString());
+			
+			final File inputFile=getInputFile(configuration);
+			
+			
 
+			
+			
 
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -43,15 +56,15 @@ public class COLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
 
 	}
 
-	private File getCOConfig(ILaunchConfiguration configuration) throws CoreException {
+	private File getInputFile(ILaunchConfiguration configuration) throws CoreException {
 		String configPath = configuration.getAttribute(INPUT_FILE, StringUtils.EMPTY);
-		try {
-			File file = new File(configPath);
-			return file;
-		} catch (NullPointerException e) {
+		java.net.URI inputFileUri=java.net.URI.create(configPath);
+		File inputFile = new File(inputFileUri);
+		if (!inputFile.isFile()){
 			throw new CoreException(new Status(IStatus.ERROR, DiceConfigurationOptimizationPlugin.PLUGIN_ID, 
-					MessageFormat.format("The config file is empty", configPath), e));
+					MessageFormat.format("The config file does not exists or the path is invalid", inputFile)));
 		}
+		return inputFile;
 	}
-
+	
 }
